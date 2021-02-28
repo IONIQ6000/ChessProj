12399f02ca45ef689b79058e91fd86965c3018c0
TheMoonSpell <50470441+TheMoonSpell@users.noreply.github.com>
2021-02-28 13:55:28 -0500

    new push

--- src/com/company/Pawn.java
+++ src/com/company/Pawn.java
@@ -5,7 +5,9 @@ public class Pawn {
 
     public static void pawnMethod() {
         System.out.println("True" + Main.pieceToMove);
-
+        int inumx = (Main.pieceToMove.charAt(1) - 'a');
+        int inumy = Integer.parseInt(Main.pieceToMove.substring(2))-1;
+        System.out.println(inumx+""+inumy);
         
     }
 }
